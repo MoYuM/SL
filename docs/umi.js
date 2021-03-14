3a9498c7bf3ef3ad6ae27d56d79c194635443d51
MoYuM <hanbaochang12138@gmail.com>
2021-03-14 19:03:56 +0800

    build

--- docs/umi.js
+++ docs/umi.js
@@ -21111,10 +21111,10 @@
     window.routerBase && (s.basename = window.routerBase);
     var f = Object({ NODE_ENV: 'production' }).__IS_SERVER
         ? null
-        : Object(l['a'])(s),
+        : Object(l['b'])(s),
       d = function () {
         var e = arguments.length > 0 && void 0 !== arguments[0] && arguments[0];
-        return e || (f = Object(l['a'])(s)), f;
+        return e || (f = Object(l['b'])(s)), f;
       };
     function p(e) {
       if (Array.isArray(e)) return e;
