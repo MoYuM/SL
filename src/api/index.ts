abdadd519ed64777a7cb935a11cf6c69d2045c2b
MoYuM <hanbaochang12138@gmail.com>
2021-03-15 23:43:38 +0800

    feat: 增加token

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,7 @@
 import { Octokit } from '@octokit/rest';
 
-const octokit = new Octokit();
+const octokit = new Octokit({
+  auth: 'd4e7ee4383953eea2607298868bdafcb30dfb3ed',
+});
 
 export default octokit;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,12 @@ import styles from './index.less';
 import octokit from '@/api';
 
 export default function IndexPage() {
+  console.log(
+    'data',
+    octokit.issues.list({
+      filter: 'created',
+    }),
+  );
   return (
     <div>
       <h1>Hello,World</h1>
